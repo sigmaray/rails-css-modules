684ee21a6cdefef5417a8621f2732ec757785f0f
Stanislav Zaleski <sigmaray@gmail.com>
2019-06-17 12:07:32 +0300

    hello-react.module.sass

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,9 +5,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import styles from '../styles/hello-react'
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <div className={styles.helloReact}>Hello {props.name}!</div>
 )
 
 Hello.defaultProps = {
